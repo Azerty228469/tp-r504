6ce55d5e8a8a5dd0f24e72539cf1efb8d15c018a
Azerty228469 <alexandre.pp85@gmail.com>
2023-09-08 13:21:22 +0200

    création fichier

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,9 +1,8 @@
 import fonctions as f
 
 print("Hello, World!")
-v1 = int(input("Inscrire un nombre entier a : "))
-v2 = int(input("Inscrire un nombre entier b : "))
-res = f.puissance(v1, v2)
-print("Nombre A élevé au carré B :", res)
-#while True:
-#	print("Boucle infinie, effectuer CTRL+C.")
+while True:
+	v1 = int(input("Inscrire un nombre entier a : "))
+	v2 = int(input("Inscrire un nombre entier b : "))
+	res = f.puissance(v1, v2)
+	print("Nombre A élevé au carré B :", res)
