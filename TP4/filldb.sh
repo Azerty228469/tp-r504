09d5e01460250a3e1c203c43d04280695c3083b1
Azerty228469 <aaa@eee.fr>
2023-10-02 09:47:39 +0200

    TP4

--- TP4/filldb.sh
+++ TP4/filldb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+set -x
+mysql -v -u root -p'foo' -h 127.0.0.1 --port=3307 < data.sql
--- TP4/run_mysql.sh
+++ TP4/run_mysql.sh
@@ -2,7 +2,7 @@
 docker run --rm -d \
 	-p 3307:3306 \
 	-v vol-sql-demo:/var/lib/mysql \
-	--name tp4−sql \
-	--env MYSQL\_ROOT\_PASSWORD=foo \
+	--name tp4-sql \
+	--env MYSQL_ROOT_PASSWORD=foo \
 	--network net-tp4 \
 	mysql
