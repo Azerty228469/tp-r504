00aa140b5e0f2e8b18a4f2e7bc11f0ca97f65d34
Azerty228469 <aaa@eee.fr>
2023-10-23 15:26:28 +0200

    TP7.3

--- TP7/creation_B.sh
+++ TP7/creation_B.sh
@@ -1,6 +1,7 @@
 while IFS=";" read v_nom v_passwd
 do
 	v_passwd=$(date +%s | sha256sum | head -c 8)
+	v_passwd=$(pwgen -1)
 	echo $v_nom:$v_passwd | sudo chpasswd
 	echo "nom=$v_nom passwd=$v_passwd"
 done < liste3.txt
