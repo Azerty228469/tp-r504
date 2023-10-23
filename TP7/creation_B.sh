6bf8482f01a9b911cf6f8607d9d9a4c4c6781844
Azerty228469 <aaa@eee.fr>
2023-10-23 15:23:02 +0200

    Modification Q3.4

--- TP7/creation_B.sh
+++ TP7/creation_B.sh
@@ -1,5 +1,6 @@
 while IFS=";" read v_nom v_passwd
 do
-	echo "nom=$v_nom passwd=$v_passwd"
+	v_passwd=$(date +%s | sha256sum | head -c 8)
 	echo $v_nom:$v_passwd | sudo chpasswd
+	echo "nom=$v_nom passwd=$v_passwd"
 done < liste3.txt
