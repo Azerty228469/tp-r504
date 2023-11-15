ef67aae88b0682f887d7ac89b270d53f179201b1
Azerty228469 <aaa@eee.fr>
2023-11-15 10:53:30 +0100

    Compte LB et test équité

--- TP-LB/tp-A/boucle.sh
+++ TP-LB/tp-A/boucle.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
-count_server_1=0
-count_server_2=0
+compte_s1=0
+compte_s2=0
 
 for i in {1..500}; do
-    response=$(curl -s http://localhost:83)
+    result=$(curl -s http://localhost:83)
     
     # Vérifier à quelle serveur la réponse appartient
-    if [[ $response == "<h1>Hello 1</h1>" ]]; then
-        ((count_server_1++))
-    elif [[ $response == "<h1>Hello 2</h1>" ]]; then
-        ((count_server_2++))
+    if [[ $result == "<h1>Hello 1</h1>" ]]; then
+        ((compte_s1++))
+    elif [[ $result == "<h1>Hello 2</h1>" ]]; then
+        ((compte_s2++))
     fi
 done
 
-echo "Nombre de réponses du serveur 1 : $count_server_1"
-echo "Nombre de réponses du serveur 2 : $count_server_2"
+echo "Nombre de réponses du serveur 1 : $compte_s1"
+echo "Nombre de réponses du serveur 2 : $compte_s2"
