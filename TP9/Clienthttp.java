4603c72d46ae24903ac4e16121da3bb62d070b59
Azerty228469 <aaa@eee.fr>
2023-11-30 09:12:53 +0100

    Client HTTP

---
+++ TP9/Clienthttp.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class Clienthttp
+{
+	public static void main( String[] args )
+	{
+		try
+		{
+			Socket socket = new Socket( args[0], 80 );
+			OutputStreamWriter osw = new OutputStreamWriter( socket.getOutputStream() );
+			InputStreamReader isw = new InputStreamReader( socket.getInputStream() );
+
+			BufferedWriter bufOut = new BufferedWriter( osw );
+			BufferedReader bufIn = new BufferedReader( isw );
+
+			String request = "GET / HTTP/1.0\r\n\r\n"; // Requête HTTP
+			bufOut.write( request, 0, request.length() );
+			bufOut.flush();
+
+			String line = bufIn.readLine(); // Lecture ligne par ligne
+			while( line != null ) {			// Tant qu'il y a des données reçues,
+				System.out.println( line );	// les afficher
+				line = bufIn.readLine();
+			}
+			bufIn.close();
+			bufOut.close();
+			socket.close();
+		}
+		catch( Exception zz)
+		{
+		}
+	}
+}
