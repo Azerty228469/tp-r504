eaf59f1268ec8ca9e0eb2b02fdbaff69a3353515
Azerty228469 <aaa@eee.fr>
2023-12-18 09:51:28 +0100

    Menu ex1

--- TP11/testutil.sh
+++ TP11/testutil.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-PS3="Sélectionnez"
-options=("1 - Vérifier l'existence d'un utilisateur" "2 - Connaître l'UID d'un utilisateur" "q - Quitter")
+PS3="Sélectionnez : "
+options=("Vérifier l'existence d'un utilisateur" "Connaître l'UID d'un utilisateur" "Quitter")
 select opt in "${options[@]}"
 do
     case $opt in
-        "1 - Vérifier l'existence d'un utilisateur")
-            echo "1"
+        "Vérifier l'existence d'un utilisateur")
+			read -p "Tapez un nom d'utilisateur : " nom
+            id -u $nom &> /dev/null
+			reponse=$?
+			if [ "$reponse" -eq 0 ]
+			then
+				echo "Utilisateur existant"
+			else
+				echo "Utilisateur inconnu"
+			fi
+            break
             ;;
-        "2 - Connaître l'UID d'un utilisateur")
-            echo "2"
+        "Connaître l'UID d'un utilisateur")
+			read -p "Tapez un nom d'utilisateur : " nom2
+            echo "ID de $nom2 : ";id -u $nom2
+            break
             ;;
-        "q - Quitter")
+        "Quitter")
+            echo "Fermeture..."
             break
             ;;
         *) echo "Option invalide $REPLY";;
     esac
 done
-
