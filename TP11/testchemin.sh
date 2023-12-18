bf8a617d858712cb4740f56f2bb15a91b75bb0c1
Azerty228469 <aaa@eee.fr>
2023-12-18 10:48:12 +0100

    Test existence chemin

---
+++ TP11/testchemin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" -gt "0" ]
+then
+	chemin_absolu="$1"
+	IFS='/' read -ra chemins <<< "$chemin_absolu"
+	chemin_partiel=""
+	for dossier in "${chemins[@]}"; do
+	    chemin_partiel+="/$dossier"
+	    if [ ! -d "$chemin_partiel" ]; then
+	        echo "Chemin invalide, le dossier $dossier n'existe pas dans $chemin_partiel"
+	        exit 1
+	    fi
+	done
+	echo "Chemin valide"
+else
+	echo "Un seul argument est requis."
+fi
